ff8c480f7ead1596553f93fafe539817867a439d
alejandro baranek <alejandrobaranek@gmail.com>
2018-03-13 09:35:00 -0300

    [bugfix] in getAvailablePolyhedra when no data for a source

--- R/zzz.R
+++ R/zzz.R
@@ -44,7 +44,7 @@
   assign(".polyhedra", value = .polyhedra, envir = parent.env(environment()))
   scrapePolyhedraSources(max.quant.config.schedule = 100,
                          max.quant.scrape = 0,
-                         time2scrape.source = 20,
+                         time2scrape.source = 5,
                          sources.config = .available.sources,
                          retry.scrape = FALSE)
 }
