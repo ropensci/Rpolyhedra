8257aeadcb3e9914a9a969e724db67718f447927
alejandro baranek <alejandrobaranek@gmail.com>
2020-07-05 13:44:10 -0300

    Release 0.4.4

--- R/package-lib.R
+++ R/package-lib.R
@@ -39,8 +39,8 @@ updatePolyhedraDatabase <- function(source.filenames = NULL){
   .available.scrapping.conf <- getUserEnvir(".available.scrapping.conf")
 
   #"dev-tetrahedron" "dev-minimal" "pkg-minimal" "fulldb"
-  #setUserEnvir(".scrape.config", "pkg-minimal")
-  setUserEnvir(".scrape.config", "fulldb")
+  setUserEnvir(".scrape.config", "pkg-minimal")
+  #setUserEnvir(".scrape.config", "fulldb")
   #When release version, change parameter to "pkg-minimal"
   .scrape.config <- getUserEnvir(".scrape.config")
   scrapePolyhedra(scrape.config = .available.scrapping.conf[[.scrape.config]],
