ba8f15aa575cfa0e6f5031949ad9a17699e844cf
alejandro baranek <alejandrobaranek@gmail.com>
2022-09-26 20:57:23 -0300

    db updated to 0.5.4 because of netlib url updated

--- R/zzz.R
+++ R/zzz.R
@@ -11,7 +11,6 @@
   # It is not necesary to put trivial compatibility
   .package.db[["0.2.5"]] <- "0.2.5"
   .package.db[["0.5.1"]] <- "0.5.0"
-  .package.db[["0.5.4"]] <- "0.5.3"
 
   setPackageEnvir(variable.name = ".package.db", .package.db)
 
